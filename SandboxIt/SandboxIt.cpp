0af893dc7f395c2665cdc00ddccbc89fafc70747
Roberto <ramado@s2grupo.es>
2022-10-03 10:25:25 +0200

    Agregar archivos de proyecto.

---
+++ SandboxIt/SandboxIt.cpp
@@ -0,0 +1,15 @@
+// SandboxIt.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <cstdio>
+#include <tlhelp32.h>
+#include <Lmcons.h>
+#include <sddl.h>
+
+int main()
+{
+
+}
+
